23afb84f96ffb1b010aabdf0dac9a5533b50bcfd
CodeNinjas-Rocklin <60720537+CodeNinjas-Rocklin@users.noreply.github.com>
2020-06-03 11:42:21 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,6 +7,27 @@ function MovePlayer () {
     }
     player.x = Clamp(player.x, 0, screen.width)
 }
+function UpdateCoins () {
+    for(let i = 0; i < coins.length; )
+    {
+        coins[i].y -= treeSpeed
+        if(i == 0 && coins[i].y < -5)
+        {
+            coins.shift().destroy()
+        }
+        else
+        {
+            if(coins[i].kind() == 0 && Collision(player, coins[i], 1))
+            {
+                info.changeScoreBy(5);
+                coins[i].setImage(blankImage)
+                coins[i].setKind(1)
+                music.powerUp.play()
+            }
+            ++i;
+        }
+    }
+}
 function CreateTrees () {
     if (treesLeft > 0) {
         treeTimer += -1
@@ -20,26 +41,58 @@ function CreateTrees () {
     }
 }
 function UpdateTrees () {
-    for (let i = 0; i <= trees.length - 1; i++) {
-        trees[i].y -= treeSpeed;
-if (trees[i].y < -30) {
-            trees[i].y = screen.height + 30
-            trees[i].x = RandomInt(screen.width)
+    for (let j = 0; j <= trees.length - 1; j++) {
+        trees[j].y -= treeSpeed
+if (trees[j].y < -30) {
+            trees[j].y = screen.height + 30
+            trees[j].x = RandomInt(screen.width)
+            trees[j].setImage(list[RandomInt(list.length)])
             info.changeScoreBy(1)
+            if (RandomInt(100) > 0) {
+                CreateCoin(trees[j])
+            }
             if (info.score() % pointsForSpeed == 0) {
                 treeSpeed += 1
                 pointsForSpeed *= 2
             }
         }
         if (iFrames == 0) {
-            if (Collision(player, trees[i], .3)) {
+            if (Collision(player, trees[j], .3)) {
                 info.changeLifeBy(-1)
                 if (info.life() > 0) {
                     iFrames = 50
+                    music.powerDown.play()
                 }
             }
         }
     }
+}
+function CreateCoin (parent: Sprite) {
+    newCoin = sprites.create(img`
+        . . . . . . 5 5 5 5 . . . . . .
+        . . . . 5 5 5 5 5 5 5 . . . . .
+        . . . 5 5 5 5 5 5 5 5 . . . . .
+        . . 5 5 5 5 5 5 5 5 5 5 . . . .
+        . . 5 5 5 5 5 5 5 5 5 5 5 . . .
+        . . 5 5 5 5 5 5 5 5 5 5 5 . . .
+        . 5 5 5 5 5 5 5 5 5 5 5 5 . . .
+        . 5 5 5 5 5 5 5 5 5 5 5 5 . . .
+        . 5 5 5 5 5 5 5 5 5 5 5 5 5 . .
+        . 5 5 5 5 5 5 5 5 5 5 5 5 5 . .
+        . 5 5 5 5 5 5 5 5 5 5 5 5 5 . .
+        . . 5 5 5 5 5 5 5 5 5 5 5 5 . .
+        . . 5 5 5 5 5 5 5 5 5 5 5 . . .
+        . . 5 5 5 5 5 5 5 5 5 5 5 . . .
+        . . . 5 5 5 5 5 5 5 5 5 . . . .
+        . . . 5 5 5 5 5 . . . . . . . .
+    `, 0)
+    newCoin.y = screen.height + 30
+    if (parent.x > screen.width / 2) {
+        newCoin.x = parent.x - 20
+    } else {
+        newCoin.x = parent.x + 20
+    }
+    coins.push(newCoin)
 }
 function FlashEffect () {
     if (iFrames > 0) {
@@ -90,7 +143,7 @@ function SetUp () {
         3 3 3 3 3 3 5 5 5 3 3 3 3 3 3 3
         3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3
     `
-    scene.setBackgroundColor(6)
+    scene.setBackgroundColor(12)
     player = sprites.create(playerSprite, 0)
     list.push(img`
         . . . . . . . . . . . . . . . . . 7 7 . . . . . . . . . . . . .
@@ -126,6 +179,40 @@ function SetUp () {
         . . . . . . . . . . . . . . e e e . . . . . . . . . . . . . . .
         . . . . . . . . . . . . . . e e e . . . . . . . . . . . . . . .
     `)
+    list.push(img`
+        . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . . 6 6 . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . 6 6 6 6 . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . 6 6 6 6 6 . . . . . . . . . . . .
+        . . . . . . . . . . . . . . 6 6 6 6 6 6 6 . . . . . . . . . . .
+        . . . . . . . . . . . . . . 6 6 6 6 6 6 6 . . . . . . . . . . .
+        . . . . . . . . . . . . . 6 6 6 6 6 6 6 6 . . . . . . . . . . .
+        . . . . . . . . . . . . 6 6 6 6 6 6 6 6 6 6 . . . . . . . . . .
+        . . . . . . . . . . . . 6 6 6 6 6 6 6 6 6 6 . . . . . . . . . .
+        . . . . . . . . . . . 6 6 6 6 6 6 6 6 6 6 6 6 . . . . . . . . .
+        . . . . . . . . . . 6 6 6 6 6 6 6 6 6 6 6 6 6 . . . . . . . . .
+        . . . . . . . . . 6 6 6 6 6 6 6 6 6 6 6 6 6 6 . . . . . . . . .
+        . . . . . . . . . 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 . . . . . . . .
+        . . . . . . . . 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 . . . . . . . .
+        . . . . . . . 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 . . . . . . .
+        . . . . . . . 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 . . . . . . .
+        . . . . . . 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 . . . . . .
+        . . . . . . 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 . . . . . .
+        . . . . . 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 . . . . .
+        . . . . . 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 . . . . .
+        . . . . 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 . . . .
+        . . . . 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 . . .
+        . . . 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 . . .
+        . . . 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 . . .
+        . . 6 6 6 6 6 6 6 6 6 6 6 e 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 . . .
+        . 6 6 6 6 6 6 6 6 6 6 6 6 6 e e e e e e 6 6 6 6 6 6 6 6 6 . . .
+        . . . . 6 6 6 6 6 6 6 6 6 e e e e e e e 6 6 6 6 6 6 6 6 . . . .
+        . . . . . . . . . . . . e e e e e e e e . . . . . . . . . . . .
+        . . . . . . . . . . . . e e e e e e e e . . . . . . . . . . . .
+        . . . . . . . . . . . . e e e e e e e e . . . . . . . . . . . .
+        . . . . . . . . . . . e e e e e e e e e . . . . . . . . . . . .
+    `)
     info.setScore(0)
     info.setLife(3)
     speed = 3
@@ -135,25 +222,28 @@ function SetUp () {
     treeTimer = 1
     pointsForSpeed = 10
     info.setBackgroundColor(9)
-    info.setFontColor(0)
-    info.setBorderColor(10)
+info.setFontColor(0)
+info.setBorderColor(10)
 }
 let playerSprite: Image = null
-let blankImage: Image = null
+let newCoin: Sprite = null
 let iFrames = 0
 let newTree: Sprite = null
 let treeTimer = 0
 let treesLeft = 0
 let player: Sprite = null
 let trees: Sprite[] = []
+let coins: Sprite[] = []
 let list: Image[] = []
 let pointsForSpeed = 0
 let treeSpeed = 0
 let speed = 0
+let blankImage: Image = null
 SetUp()
 game.onUpdate(function () {
     MovePlayer()
     CreateTrees()
     UpdateTrees()
+    UpdateCoins()
     FlashEffect()
 })
